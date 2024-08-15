bbdab2298f5abd8fc27ac6bacb07564a76ea4848
Lucas01v <lucasvivanco01@outlook.com>
2024-08-15 15:23:28 -0300

    feat(productos): agregar carrusel de productos de temporada a homepage

---
+++ src/components/common/ProductosTemporada.jsx
@@ -0,0 +1,93 @@
+import { useState, useEffect } from 'react';
+import Slider from 'react-slick';
+import { useNavigate } from 'react-router-dom';
+import styles from './ProductosDestacados.module.css';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const ProductosTemporada = () => {
+    const [seasonalProducts, setSeasonalProducts] = useState([]);
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const fetchProducts = async () => {
+            try {
+                const response = await fetch('http://localhost:5000/admin/products/carousel/seasonal');
+                const data = await response.json();
+                setSeasonalProducts(data.slice(0, 10)); // Obtener los primeros 10 productos para el slider
+            } catch (error) {
+                console.error('Error fetching products:', error);
+            }
+        };
+
+        fetchProducts();
+    }, []);
+
+    const settings = {
+        dots: true,
+        infinite: seasonalProducts.length > 1, //evita la repetición de elementos
+        speed: 500,
+        slidesToShow: Math.min(4, seasonalProducts.length),
+        slidesToScroll: 1,
+        nextArrow: <div className="slick-next" />,
+        prevArrow: <div className="slick-prev" />,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    };
+
+    const handleProductClick = (id) => {
+        navigate(`/producto/${id}`);
+    };
+
+    const handleViewMoreProducts = () => {
+        navigate('/bebes');
+    };
+
+    return (
+        <div className={styles.container}>
+            <h1 className={styles.title}>Productos Destacados</h1>
+            <Slider {...settings}>
+                {seasonalProducts.map(product => (
+                    <div 
+                        key={product._id} 
+                        className={styles.productCard}
+                    >
+                        <img src={product.imagenes[0]} alt={product.nombre} className={styles.productImage} />
+                        <h2 className={styles.productName}>{product.nombre}</h2>
+                        <p className={styles.productPrice}><span>${product.precio.toFixed(2)}</span> ${product.promocion.toFixed(2)}</p>
+                        <button className={styles.viewProductButton} onClick={() => handleProductClick(product._id)}>VER PRODUCTO</button>
+                        <button className={styles.addButton}>AGREGAR AL CARRO</button>
+                    </div>
+                ))}
+            </Slider>
+            <div className={styles.viewMoreContainer}>
+                <button className={styles.viewMoreButton} onClick={handleViewMoreProducts}>VER MÁS PRODUCTOS</button>
+            </div>
+        </div>
+    );
+};
+
+export default ProductosTemporada;
