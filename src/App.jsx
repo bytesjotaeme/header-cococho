3fbfcdad1fdf41671283f38200d4ebdba06d620d
Lucas01v <lucasvivanco01@outlook.com>
2024-08-16 23:05:27 -0300

    fix: agegar rutas de carruseles en homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+// import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css'; // Importa los estilos de Bootstrap
+// import { CartProvider } from './components/CartContext'; // Actualiza la ruta si es necesario
 import { CartProvider } from './contexts/CartContext';
 import Header from './components/common/Header';
 import Footer from './components/common/Footer'; // Importa el Footer aquí
@@ -12,7 +13,7 @@ import Ninos from './pages/Ninos/Ninos';
 import Accesorios from './pages/Accesorios/Accesorios';
 import Juguetes from './pages/Juguetes/Juguetes';
 import ProductDetails from './pages/ProductDetails/ProductDetails';
-import Cart from './components/common/Cart';
+import CartModal from './components/CartModal'; // Importa el CartModal
 import './assets/styles/global.css';
 import ProductosDestacados from './components/common/ProductosDestacados';
 import PublicidadSlider from './components/common/PublicidadSlider';
@@ -20,6 +21,8 @@ import Categorias from './components/common/Categorias';
 import ProductAdmin from './pages/Admin/ProductAdmin';
 import ProductEdit from './components/Admin/ProductEdit';
 import ProductAdd from './components/Admin/ProductAdd';
+import ProductosOferta from './components/common/ProductosOferta';
+import ProductosTemporada from './components/common/ProductosTemporada';
 
 function App() {
     return (
@@ -27,18 +30,20 @@ function App() {
             <Router>
                 <div className="App">
                     <Header />
-                    <Cart />
+                    <CartModal /> {/* Agrega el CartModal aquí para que esté disponible en todas las páginas */}
                     <Routes>
                         <Route path="/" element={
                             <>
                                 <PromotionsSlider />
+                                <ProductosOferta/>
+                                <ProductosTemporada/>
                                 <ProductosDestacados />
                                 <Categorias />
                                 <PublicidadSlider />
                                 <Newsletter />
                             </>
                         } />
-                        <Route path="/cart" element={<Cart asPage={true} />} />
+                        <Route path="/cart" element={<CartModal asPage={true} />} /> {/* Ruta específica para mostrar el carrito como página */}
                         <Route path="/bebes" element={<Bebes />} />
                         <Route path="/ninas" element={<Ninas />} />
                         <Route path="/ninos" element={<Ninos />} />
