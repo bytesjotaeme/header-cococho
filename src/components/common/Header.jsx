9752ba27e5a3dd60a18aa890632bee4199f77f65
Lucas01v <lucasvivanco01@outlook.com>
2024-08-26 22:49:30 -0300

    fix (nav): cambiar links de redirección por botones y estilos para los mismos

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import './Header.css';
 import logo from '../../assets/images/Daraz-logo.png'; // Ajusta la ruta según la ubicación del logo
 import CartButton from '../cart/CartButton';
 
 const Header = () => {
+    const navigate = useNavigate();
+
+    const handleCategoryClick = (category) => {
+        navigate(`/products/category/${category}`);
+    };
+
     return (
         <header className="header">
             <div className="header__logo">
@@ -12,26 +18,21 @@ const Header = () => {
             </div>
             <nav className="header__nav">
                 <Link to="/">INICIO</Link>
-                <Link to="/bebes">BEBÉS</Link>
-                <Link to="/ninas">NIÑAS</Link>
-                <Link to="/ninos">NIÑOS</Link>
-                <Link to="/accesorios">ACCESORIOS</Link>
-                <Link to="/juguetes">JUGUETES</Link>
+                <button onClick={() => handleCategoryClick('bebé')}>BEBÉ</button>
+                <button onClick={() => handleCategoryClick('beba')}>BEBA</button>
+                <button onClick={() => handleCategoryClick('niñas')}>NIÑAS</button>
+                <button onClick={() => handleCategoryClick('niños')}>NIÑOS</button>
+                <button onClick={() => handleCategoryClick('accesorios')}>ACCESORIOS</button>
+                <button onClick={() => handleCategoryClick('juguetes')}>JUGUETES</button>
+                <button onClick={() => handleCategoryClick('Rodados')}>RODADOS</button>
             </nav>
             <div className="header__actions">
                 <input type="text" placeholder="Search" className="header__search" />
                 <Link to="/login" className="header__login">Iniciar sesión</Link>
                 <CartButton/>
-                {/* <div className="header__cart">
-                    <Link to="/cart">
-                        <span className="header__cart-icon"></span>
-                        <span className="header__cart-count">0</span>
-                    </Link>
-                </div> */}
             </div>
         </header>
     );
 };
 
 export default Header;
-
