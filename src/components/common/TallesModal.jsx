8afaeace98d97ba1008272b94be37fb1fbe7f79c
Lucas01v <lucasvivanco01@outlook.com>
2024-09-03 23:51:34 -0300

    feat (talles): añadir modal con talles para prendas

--- src/components/common/TallesModal.jsx
+++ src/components/common/TallesModal.jsx
@@ -2,21 +2,22 @@
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import TallesImage from '../../assets/images/Talles/Guia_de_talles.jpeg';
+import './TallesModal.css';
 
 function TallesModal({show, handleClose}) {
 
   return (
     <>
-      <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
+      <Modal className='modal-talles' show={show} onHide={handleClose}>
+        <Modal.Header className='modal-talles-header' closeButton>
           <Modal.Title>Guía de talles</Modal.Title>
         </Modal.Header>
-        <Modal.Body>
+        <Modal.Body className='modal-body-talles'>
            <img src={TallesImage} alt="Guía de talles" />
         </Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
-            Close
+          <Button variant="success" onClick={handleClose}>
+            Cerrar
           </Button>
         </Modal.Footer>
       </Modal>
